f8dd95bb602cbf1512085bfb7a82dd1397c57ec5
Mahsa <mahsafaateh@gmail.com>
2025-06-16 16:24:36 +0200

    Initial Commit

---
+++ Threadsy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Threadsy
+//
+//  Created by Mahsa Fateh on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Threadsy/ThreadsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsyApp.swift
+//  Threadsy
+//
+//  Created by Mahsa Fateh on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
